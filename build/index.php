ea17654d78df8c29d7e3c95c9aec8d54aacb18c1
A1exMorris <wertava@gmail.com>
2018-05-04 01:14:34 +0300

    parallax optimization

--- build/index.php
+++ build/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" href="assets/i/sync.ico">
 
     <title>Starter Template for Bootstrap</title>
 
@@ -20,39 +20,41 @@
   </head>
 
   <body>
-    <div class="container">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+
+    <nav class="navbar navbar-expand navbar-dark bg-primary">
       <a class="navbar-brand h1" href="#">NP</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse justify-content-center" id="navbarsExampleDefault">
+        <ul class="navbar-nav ">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Обо мне<span class="sr-only">(current)</span></a>
+                <a class="nav-link h1" href="#">Обо мне<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Портфолио</a>
+                <a class="nav-link h1" href="#">Портфолио</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Творчество</a>
+                <a class="nav-link h1" href="#">Творчество</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">Контакты</a>
+                <a href="#" class="nav-link h1">Контакты</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Поиск" aria-label="Поиск">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button>
-        </form>
+
       </div>
-    </nav>
-</div> <!--Navbar-->
+    </nav><!--Navbar-->
 
-        <div class="jumbotron paral paralsec1">
-            <p class="h1">ЧТО то своеЁ</p>
-
+        <div class="jumbotron paral paralsec1 d-flex justify-content-center">
+            <div class="container flex-auto align-self-center" >
+           <div class="mystyle"> <p class="h1">ЧТО то своеЁ</p>
+                <p class="h1">ЧТО то своеЁ</p>
+                <p class="h1">ЧТО то своеЁ</p>
+                <p class="h1">ЧТО то своеЁ</p>
+                <p class="h1">ЧТО то своеЁ</p>
+               </div>
+            </div>
         </div>
     <div class="container">
         <p class="h1">dddddddddddd</p>
